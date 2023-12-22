8263a2246fc4ed79063012feef01250b2a9aa1bd
Peter Zheng <35768272+PandaPilot@users.noreply.github.com>
2023-12-22 16:47:09 +0000

    nav highlighting

--- scripts/local.js
+++ scripts/local.js
@@ -65,14 +65,28 @@ myCarousel.addEventListener('slid.bs.carousel', function () {
     });
 });
 
-/*
-document.addEventListener('DOMContentLoaded', function() {
-    if ('ontouchstart' in window) {
-        // Touch support detected
-        var carousel = document.getElementById('myCarousel'); // Adjust ID if needed
-        if (carousel) {
-            carousel.classList.add('touch-screen');
-        }
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll('.scrolly');
+    const sections = document.querySelectorAll('section');
+
+    // Function to update active link
+    function updateActiveLink() {
+        let index = sections.length;
+
+        while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+        links.forEach((link) => link.classList.remove('active'));
+        links[index].classList.add('active');
     }
+
+    // Event listener for click
+    links.forEach(link => {
+        link.addEventListener('click', function() {
+            links.forEach(lnk => lnk.classList.remove('active'));
+            this.classList.add('active');
+        });
+    });
+
+    // Event listener for scroll
+    window.addEventListener('scroll', updateActiveLink);
 });
-*/
