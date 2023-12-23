73e727d01070087955aac7a264f8a047c14e1fcb
Peter Zheng <35768272+PandaPilot@users.noreply.github.com>
2023-12-23 15:17:24 +0000

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -1,4 +1,4 @@
-document.getElementById('video').addEventListener('click', function() {
+document.getElementById('cinema-container').addEventListener('click', function() {
     var container = document.getElementById('cinema-container');
     container.classList.toggle('cinema-mode');
 });
