4dd4bdfe3cb75429578e35d998d12b92d2135691
Peter Zheng <35768272+PandaPilot@users.noreply.github.com>
2023-12-23 15:03:35 +0000

    implement yt video cinema mode

---
+++ js/projects.js
@@ -0,0 +1,4 @@
+document.getElementById('video').addEventListener('click', function() {
+    var container = document.getElementById('cinema-container');
+    container.classList.toggle('cinema-mode');
+});
